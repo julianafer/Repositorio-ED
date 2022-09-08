e314107d6c89debe27b50d67c1af267fd27cffef
Juliana Ferreira <julianafercavalcante@hotmail.com>
2022-09-07 23:20:44 -0300

    códigos das vídeo aulas

--- Videoaulas/exemplo-12excessoes.py
+++ Videoaulas/exemplo-12excessoes.py
@@ -1,17 +1,46 @@
 #programa principal
 
 def lerConteudo(fileName):
-    stream = open(fileNane, 'rt')
+    stream = open(fileName, 'rt')
     linhas = stream.readlines()
     stream.close()
     return linhas
 
-while True:
-    print('''
-    (1) Erro de lógica de Programação
-    (2) Erros de condição do ambiente de execução do software
-    (3) Erros graves que não permitem recuperação
-    --------------------------------------------------''')
-    opcao = int(input('opção: '))
+try:
+    while True:
+        print('''
+        (1) Erro de lógica de Programação
+        (2) Erros de condição do ambiente de execução do software
+        (3) Erros graves que não permitem recuperação
+        --------------------------------------------------''')
+        opcao = int(input('opção: '))
 
-    
+        if opcao == 1: #Erro tipo 1
+            #limite de um array estourado
+            notas = [5,3,2,1,4]
+            print(notas)
+            index = int(input('Digite o índice de acesso ao array: '))
+            print('Nota exibida:', notas[index])
+
+        elif opcao == 2:
+            #abrir um arquivo não encontrado
+            arquivo = input('Informe o caminho e o nome do arquivo: ')
+            print(lerConteudo(arquivo))
+
+        elif opcao == 3:
+            print('Não é interessante simular....')
+            print('Exemplo: falta de memória, erro inetrno do interpretador')
+        
+        else:
+            break
+
+except IndexError:
+    print('Digite um índice válido para acessar o array')
+except ValueError:
+    print('Digite um número inteiro.')
+except Exception as e:
+    print('Mensagem:',e)
+    print('Classe da excessão:', e.__class__)
+
+
+print('\n###### Chegamos ao final do programa ######')
